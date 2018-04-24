c265cc03f499a3e1c3259bad3714affb24bfe69c
Yuriy Mironov <git>
2018-04-24 14:46:59 +0400

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = function(){
+    return{
+        products:[
+            {id:1, name:"Лодка", category:"Водный спорт", description:"Одноместная лодка", price:200},
+            {id:2, name:"Надувной матрас", category:"Водный спорт", description:"Матрас для плавания", price:250},
+            {id:3, name:"Мяч", category:"Футбол", description:"Футбольный мяч", price:100},
+            {id:4, name:"Бутсы", category:"Футбол", description:"Обувь для футбола", price:200},
+            {id:5, name:"Майка", category:"Футбол", description:"Одежда для футбола", price:250},
+            {id:6, name:"Шахматы", category:"Настольные игры", description:"Игра в шахматы", price:300},
+            {id:7, name:"Нарды", category:"Настольные игры", description:"Игра в нарды ", price:350},
+            {id:8, name:"Монополия", category:"Настольные игры", description:"Игра в монополию", price:400},
+            {id:9, name:"Манчкин", category:"Настольные игры", description:"Игра в манчкин", price:1200},
+            {id:10, name:"Го", category:"Настольные игры", description:"Игра в го", price:2550}
+
+        ],
+        orders:[]
+    }
+}
